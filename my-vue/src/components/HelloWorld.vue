3cd2a87c0364172e9d73c800c6816448b9973afe
lakshmi-500apps <lakshmi.lagadapati@500apps.com>
2022-03-22 19:18:53 +0530

    importing files

---
+++ my-vue/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="hello">
+   <whoprog></whoprog>
+   <hi></hi>
+  </div>
+</template>
+
+<script> 
+import whoprog from "./first.vue"
+import hi from "../second.vue"
+export default {
+  name: 'HelloWorld',
+  
+  components: {
+   whoprog,hi
+  },
+
+  props: {
+    msg: String
+  }
+}
+</script>
+
---
+++ my-vue/src/second.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+This is second vue file
+</div>
+</template>
+<script>
+export default{
+    name:'GrT'
+}
+
+</script>
