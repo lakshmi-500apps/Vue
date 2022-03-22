265fe25fd9789a133e267caa45ab7bd20cc382bf
lakshmi-500apps <lakshmi.lagadapati@500apps.com>
2022-03-22 15:33:40 +0530

    commit1

---
+++ first.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <p>Hello World!</p>
+</div>
+</template>
+<script>
+
+export default{
+    name:vue-example,
+    data(){
+        return{
+            fname:"chaitrika",
+            lname:"lagadapati",
+            fullname:this.fname+" "+this.lname
+        }
+        },
+        created(){
+        
+        },
+        mounted(){
+
+        },
+        methods:{
+             f1(){
+                 console.log(fullname);
+             }
+        }
+    }
+</script>
