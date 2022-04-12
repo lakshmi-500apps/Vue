96fc2a65afcc7b34584ac752f7240a13b18c1f9b
lakshmi-500apps <102018091+lakshmi-500apps@users.noreply.github.com>
2022-04-12 18:48:08 +0530

    Add files via upload

---
+++ q4.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+<h2>Question 4</h2>
+<div>
+<input placeholder="Enter text">
+</div>
+<b-button @click="getData()">submit</b-button>
+<div>
+{{ searchData }}
+</div>
+</div>
+</template>
+<script>
+var axios = require("axios").default;
+export default {
+name: "sampleApis",
+data() {
+return {
+options: {
+method: "GET",
+url: "http://www.json-generator.com/api/json/get/bSldINskRK",
+data: { search: "" },
+},
+text: "",
+searchData: {},
+};
+},
+methods: {
+async getData() {
+this.options.data.search = this.text;
+this.searchData = await axios.request(this.name);
+},},};
+</script>
