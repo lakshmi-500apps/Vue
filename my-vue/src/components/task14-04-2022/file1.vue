f135c5e2442fe1944f1c5873f6f3147c80ccecf0
lakshmi-500apps <lakshmi.lagadapati@500apps.com>
2022-04-14 19:06:05 +0530

    assignment7 cmt

---
+++ my-vue/src/components/task14-04-2022/file1.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <h1>question 1</h1>
+    <p>The output for diameter() will be 40 if called because radius variable is assigned 20 value 
+        so it will be multiplied to 2 and will return the answer.
+
+        Here perimeter is an arrow function and it will return an error because return keyword is missing
+        and syntax is not followed 
+    </p>
+</div>
+</template>
+<script>
+export default{
+    name:"QueS1"
+}
---
+++ my-vue/src/components/task14-04-2022/file9.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <h1>question 9</h1>
+    <button @click="f1()">Submit</button>
+</div>
+</template>
+<script>
+export default{
+    name:"QueS9",
+    data(){
+        return{
+            array1 : [1,0,2,3,4],
+            array2 :[3,5,6,7,8,13]
+            }
+    },
+    methods:{
+        
+        f1(){
+           for(let i=0;i<this.array1.length-1;i++){
+            for(let j=0;j<this.array2.length-1;j++){
+                console.log(this.array1[i]+this.array2[j]),
+                i++                
+            }
+           }
+           }
+        }    
+    }
+</script>
